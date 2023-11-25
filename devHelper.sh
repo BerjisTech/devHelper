9d0e1b66f69434b757b04a3ae650ad8bea7a08f9
Prometheus <berjistechnologies@gmail.com>
2023-11-25 16:53:19 +0300

    Improve SSH agent reset script for better reliability and clarity
    - Encapsulate command substitution in quotes to prevent word splitting.
    - Clear existing identities before adding a new one with ssh-add -D.
    - Replace echo -e with printf for improved portability across different systems.
    - Correct the user prompt to reflect actual script options.
    - Use printf for consistent formatting and output control.

--- devHelper.sh
+++ devHelper.sh
@@ -4,37 +4,39 @@
 
 # ANSI Color Codes
 RED='\033[0;31m'
+BLUE='\033[0;34m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
 # Reset SSH Agent
 ssh_reset() {
-    echo -e "Running ${YELLOW} eval `ssh-agent -s` ${NC} ..."
-    eval `ssh-agent -s`
-    echo -e "Running ${YELLOW} ssh-add ~/.ssh/id_rsa ${NC} ..."
+    printf "Running ${YELLOW}%s${NC} ...\n" "eval \$(ssh-agent -s)"
+    eval "$(ssh-agent -s)"
+    ssh-add -D # Clear all identities from the agent first
+    printf "Running ${YELLOW}%s${NC} ...\n" "ssh-add ~/.ssh/id_rsa"
     ssh-add ~/.ssh/id_rsa
-    echo ""   
+    printf "\n"
 }
 
 # Main menu loop
 while true; do
-    echo -e "${GREEN}Choose an option:${YELLOW}"
-    echo -e "1) Reset SSH Agent"
-    echo -e "q) Exit${NC}"
+    printf "${GREEN}Choose an option:${YELLOW}\n"
+    printf "1) Reset SSH Agent\n"
+    printf "q) Exit${NC}\n"
 
-    read -p "Enter your choice [1-6]: " choice
+    read -p "Enter your choice [1 or q]: " choice
 
     case $choice in
         1)
             ssh_reset
             ;;
         q)
-            echo "Exiting..."
+            printf "Exiting...\n"
             exit 0
             ;;
         *)
-            echo "Invalid choice."
+            printf "${RED}Invalid choice.${NC}\n"
             ;;
     esac
 done
